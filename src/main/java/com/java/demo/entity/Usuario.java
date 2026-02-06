96b52f3edafad5309a52dbb31e09bbf0692b5944
Julio Cesar S Freitas <julio.c.freitas@bradesco.com>
2026-02-06 00:43:40 -0300

    Projeto Spring Boot - Park api

---
+++ src/main/java/com/java/demo/entity/Usuario.java
@@ -0,0 +1,67 @@
+package com.java.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "data_criacao")
+    private LocalDate dataCriacao;
+
+    @Column(name = "data_modificacao")
+    private LocalDate dataModificacao;
+
+    @Column(name = "criado_por", length = 100)
+    private String criadoPor;
+
+    @Column(name = "modificado_por", length = 100)
+    private String modificadoPor;
+
+
+    private enum Role {
+        ROLE_ADMIN, ROLE_CLIENTE
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
+    }
+}
